e9197d0898df5a12a1ff3fea3d8a304fd00f6815
Mary <of51@mail.ru>
2023-03-11 10:26:47 +0300

    Добавила блок-схему

--- Verification work/Program.cs
+++ Verification work/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+string[] array1 = new string[5] {"1234", "1567", "-2", "computer science", "56"};
+string[] array2 = new string[array1.Length];
